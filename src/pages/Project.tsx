2efdc6768e51ada88c2220da9d1a8c8edb656d0b
May <maykhine.ada@gmail.com>
2023-08-13 21:00:12 -0400

    add img slide

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -2,11 +2,22 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { useLocation } from "react-router-dom";
 import projectsData from "../assets/projectsData.json";
-// import { useNavigate } from "react-router-dom";
 import Link from "@mui/material/Link";
-import { motion } from "framer-motion";
-// import Button from "@mui/material/Button";
-// import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { wrap } from "popmotion";
+import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import { Card, CardMedia, CardContent, CardAction } from "@mui/material";
+// import CardMedia from "@mui/material/CardMedia";
+// import CardContent from "@mui/material/CardContent";
+// import CardActions from "@mui/material/CardActions";
+export const images = [
+  "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
+  "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
+  "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png",
+];
+
 export const Project = () => {
   const location = useLocation();
   const projectName = location.pathname
@@ -16,12 +27,12 @@ export const Project = () => {
   const curProj: JSON = projectsData[projectName];
   console.log("projectdata: ", curProj);
 
-  //   const navigate = useNavigate();
-  //   const liveLink = () => {
-  //     console.log("live link is clicked");
-  //     window.location.href = curProj.git;
-  //   };
+  const [[img, direction], setImg] = useState([0, 0]);
+  const imageIndex = wrap(0, images.length, img);
 
+  const paginate = (newDirection: number) => {
+    setImg([img + newDirection, newDirection]);
+  };
   return (
     <div>
       <motion.div
@@ -30,34 +41,96 @@ export const Project = () => {
         transition={{ duration: 1 }}
       >
         <Box sx={{ margin: "20px" }}>
-          <Link
-            href={curProj.git}
-            underline="none"
-            color="inherit"
-            target="_blank"
-            rel="noopener noreferrer "
-          >
-            <Typography variant="h4" gutterBottom>
-              {curProj.name}
-            </Typography>
-          </Link>
-          <Typography variant="h6" gutterBottom>
-            {curProj.desc}
-          </Typography>
-          <Typography variant="subtitle2" gutterBottom>
-            {curProj.technology}
-          </Typography>
-          <Link
-            href={curProj.git}
-            underline="none"
-            color="blue"
-            target="_blank"
-            rel="noopener noreferrer "
-          >
-            <Typography variant="subtitle2" gutterBottom>
-              Source Code
-            </Typography>
-          </Link>
+          <Card>
+            <CardMedia>
+              <AnimatePresence initial={false} custom={direction}>
+                <motion.img
+                  key={img}
+                  src={images[imageIndex]}
+                  custom={direction}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { type: "spring", stiffness: 300, damping: 30 },
+                    opacity: { duration: 0.2 },
+                  }}
+                  drag="x"
+                  dragConstraints={{ left: 0, right: 0 }}
+                  dragElastic={1}
+                  onDragEnd={(e, { offset, velocity }) => {
+                    const swipe = Math.abs(offset.x) * velocity.x;
+                    if (swipe < -100000) {
+                      paginate(1);
+                    } else if (swipe > 100000) {
+                      paginate(-1);
+                    }
+                  }}
+                />
+              </AnimatePresence>
+              <ArrowBackIosRoundedIcon
+                onClick={() => paginate(1)}
+              ></ArrowBackIosRoundedIcon>
+              <ArrowForwardIosRoundedIcon
+                onClick={() => paginate(-1)}
+              ></ArrowForwardIosRoundedIcon>
+            </CardMedia>
+            <CardContent>
+              <Link
+                href={curProj.url}
+                underline="none"
+                color="inherit"
+                target="_blank"
+                rel="noopener noreferrer "
+              >
+                <Typography variant="h4" gutterBottom>
+                  {curProj.name}
+                </Typography>
+              </Link>
+              <Typography variant="h6" gutterBottom>
+                {curProj.desc}
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom>
+                {curProj.technology}
+              </Typography>
+
+              <div style={{ display: "flex" }}>
+                {curProj.url && (
+                  <>
+                    <Link
+                      href={curProj.url}
+                      underline="none"
+                      color="blue"
+                      target="_blank"
+                      rel="noopener noreferrer "
+                    >
+                      <Typography variant="subtitle2" gutterBottom>
+                        Live Link
+                      </Typography>
+                    </Link>
+                    <Typography
+                      style={{ marginLeft: "10px", marginRight: "10px" }}
+                    >
+                      {" "}
+                      |{" "}
+                    </Typography>
+                  </>
+                )}
+
+                <Link
+                  href={curProj.git}
+                  underline="none"
+                  color="blue"
+                  target="_blank"
+                  rel="noopener noreferrer "
+                >
+                  <Typography variant="subtitle2" gutterBottom>
+                    Source Code
+                  </Typography>
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
         </Box>
       </motion.div>
     </div>
